aa8c3fa88f6f6bd8c2a45fd3b660537e23266764
1c7 <chengzheng.apply@gmail.com>
2015-10-02 00:45:02 +0800

    ofequhq

---
+++ 3. Selection Sort/P_SelectionSort.py
@@ -0,0 +1,22 @@
+
+
+a = [31, 98, 6, 22, 1]
+print()
+print(a)
+
+
+def s_sort(l):
+  arr_len = len(l)
+  for index in range(0, arr_len):
+    # print(index)
+  
+
+
+  return 1
+
+
+b = s_sort(a)
+
+print('----Select Sort done----')
+print(b)
+print()
