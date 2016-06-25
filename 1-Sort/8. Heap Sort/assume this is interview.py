8a23d91d9c41900fd57100ec6f62e38e39efe009
1c7 <guokrfans@gmail.com>
2016-06-25 04:16:54 -0700

    change a lot, do not remember

---
+++ 1-Sort/8. Heap Sort/assume this is interview.py
@@ -0,0 +1,52 @@
+#
+# hi here is array [24, 2, 58, 16, 40, 20, 80]
+# please write a Heap sort
+#
+a = [24, 2, 58, 16, 40, 20, 80]
+#print(a)
+#print()
+
+
+
+def swap(i, j):                    
+    sqc[i], sqc[j] = sqc[j], sqc[i] 
+
+def heapify(end,i):   
+    l=2 * i + 1  
+    r=2 * (i + 1)   
+    max=i   
+    if l < end and sqc[i] < sqc[l]:   
+        max = l   
+    if r < end and sqc[max] < sqc[r]:   
+        max = r   
+    if max != i:   
+        swap(i, max)   
+        heapify(end, max)   
+
+def heap_sort():     
+    end = len(sqc)   
+    start = end // 2 - 1 # use // instead of /
+    for i in range(start, -1, -1):   
+        heapify(end, i)   
+    for i in range(end-1, 0, -1):   
+        swap(i, 0)   
+        heapify(i, 0)   
+
+sqc = [2, 7, 1, -2, 56, 5, 3]
+heap_sort()
+print(sqc)
+
+
+
+
+
+#print()
+#print(a)
+
+
+
+
+
+
+
+
