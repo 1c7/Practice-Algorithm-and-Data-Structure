f782e4061af0add902828273792ce607c6d48fb2
1c7 <guokrfans@gmail.com>
2016-06-25 23:17:43 -0700

    heap sort done

---
+++ 1-Sort/8. Heap Sort/What is Heap.md
@@ -0,0 +1,83 @@
+### Heap 是什么？
+    Heap 有两种，一种是指内存分配的，一种是数据结构的.  
+    这里讲的是数据结构的那种  
+    
+
+### Heap (数据结构)
+    一种二叉树  
+    所以树里的每个节点最多 2 个子节点，不能更多  
+
+
+### 和二叉树(每个节点最多２个子节点)到底什么区别？  
+     数字的排布方式，  
+     如果全部节点都比直属下级大，就叫 max heap，
+    (注意两个子节点不用管顺序, 比如节点 8 左子节点是 2, 右子节点是 3. 或者节点 8 左子节点是 3, 右子节点是 2. 两种情况都是合法的 max heap)  
+     如果全部节点都比直属下级小，就叫 min heap  
+     min-heap 和 max-heap，没有第三种。  
+
+  注意，如下这种情况是允许的，因为 75 不是 70 的直属子节点
+  ![screenshot](allow.png) <br/>
+
+
+### Heap 可以用来干嘛？
+     Heap Sort 和 Priority Queue
+
+### Heap 怎么表示的？
+     数组:从上到下从左到右  
+     ![screenshot](heap order.png) <br/>
+
+### Heap 的操作：
+     1 插入  
+     2 删除，（返回最大元素）  
+
+
+### 怎么把一个乱序数组变成 Max Heap？ 
+
+最大堆的插入：<br/>
+     从底部插入，向上比较和交换，　　<br/>
+     如果比上面大，就和上面交换，然后再和上面比较，直到碰到比上面小的，或者变成了 root，　　<br/>
+
+最大堆的删除：<br/>
+     假设要删除 root, 就把root 和最后一个叶节点交换，然后删掉 root<br/>
+     这个叶节点可能会比直属两个节点小，所以要进行比较和交换，最后把这个叶节点正确归位。<br/>
+     先比较左右两个节点哪个大，因为大的要排上面，然后大的和叶节点交换。<br/>
+     叶节点现在在新位置了，再次和直属两个子节点比较。遵循一样的流程。<br/>
+
+  
+
+
+
+
+
+
+
+<br/>
+### 必看资料，形象化展示了堆
+https://www.cs.usfca.edu/~galles/visualization/HeapSort.html  
+https://www.youtube.com/watch?v=WCm3TqScBM8  
+
+
+
+<br/>
+#### 参考资料
+1   
+https://courses.cs.washington.edu/courses/cse373/06sp/handouts/lecture10.pdf  
+
+2   
+Presumably you mean heap from a memory allocation point of view, not from a data structure point of view (the term has multiple meanings).  
+    http://stackoverflow.com/questions/2308751/what-is-a-memory-heap  
+
+3  
+https://github.com/raywenderlich/swift-algorithm-club/tree/master/Heap  
+
+http://stackoverflow.com/questions/749199/when-would-i-want-to-use-a-heap   
+
+4   
+http://www.csanimated.com/animation.php?t=Heap_%28data_structure%29  
+
+
+
+
+
+
+
--- 1-Sort/8. Heap Sort/assume this is interview.py
+++ 1-Sort/8. Heap Sort/assume this is interview.py
@@ -1,47 +1,174 @@
 #
-# hi here is array [24, 2, 58, 16, 40, 20, 80]
-# please write a Heap sort
+# here is an array [2,7,26,25,19,17,1,90,3,36]
+# please turn it into "Max Heap"  MAX!
+# and write a heap sort base on max heap (number from big to smaller-)
 #
-a = [24, 2, 58, 16, 40, 20, 80]
-#print(a)
-#print()
+# if you heap index start from 0
+  # by parent index, get left child node  = 2 * parent index + 1
+  # by parent index, get right child node = 2 * parent index + 2
+  # by child index, get parent node index = child index - 1 // 2
+
+# if your heap index start from 1
+  # parent of node at K is K / 2
+  # child of node at K are 2k and 2k+1
+
+#
+# if you understand what is heap, and how heap insert, heap delete work
+# then this code would be easy to understand
+# otherwise, not so much.
+#
+a = [2,7,26,25,19,17,1,90,3,36]
+print(a)
+print()
+
+
+def heap_insert(heap, i):
+  print('append', i)
+
+  if len(heap) == 0: # heap empty? just append and return
+    heap.append(i)
+    print(heap)
+    return
+
+  heap.append(i) # append first, now it is at end of heap array
+  
+  new_e_index = len(heap)-1 # get new element index
+  parent_index = (new_e_index - 1) // 2  # get parent node index
+ 
+  while heap[new_e_index] > heap[parent_index]: # new node bigger then parent?
+    # change this compare > 
+    # you can get max heap or min heap
+
+    heap[new_e_index], heap[parent_index] = heap[parent_index], heap[new_e_index]
+    # swap with parent
+    
+    # get new position index
+    new_e_index = parent_index  # now our element in parent position
+    if new_e_index == 0:
+      break
+
+    # get new parent index
+    parent_index = (new_e_index - 1) // 2
+
+    # swap with parent until it become root
+    # or until it smaller then parent node
+
+  print(heap)
+
+
+#
+# start building heap
+#
+def heapify(a):
+  heap = [] # empty heap at begining
+  print(heap)
+  for i in a:
+    heap_insert(heap, i)
+  print()
+  print('max heap:', heap)
+  return heap
+
+
+heap = heapify(a)
 
 
 
-def swap(i, j):                    
-    sqc[i], sqc[j] = sqc[j], sqc[i] 
-
-def heapify(end,i):   
-    l=2 * i + 1  
-    r=2 * (i + 1)   
-    max=i   
-    if l < end and sqc[i] < sqc[l]:   
-        max = l   
-    if r < end and sqc[max] < sqc[r]:   
-        max = r   
-    if max != i:   
-        swap(i, max)   
-        heapify(end, max)   
-
-def heap_sort():     
-    end = len(sqc)   
-    start = end // 2 - 1 # use // instead of /
-    for i in range(start, -1, -1):   
-        heapify(end, i)   
-    for i in range(end-1, 0, -1):   
-        swap(i, 0)   
-        heapify(i, 0)   
-
-sqc = [2, 7, 1, -2, 56, 5, 3]
-heap_sort()
-print(sqc)
+# =================================================
+#
+# now we have a heap, then we can use heap sort.
+# 
+# =================================================
 
 
+def max_heap_remove():
+
+  if len(heap) == 0:
+    return False
+
+  if len(heap) == 1:
+    return heap[0]
+
+  if len(heap) == 2: # if there are only two elmenet, return & remove bigger one
+    if (heap[0] > heap[1]):
+      max = heap[0]
+      heap.pop(0)
+      return max
+    else:
+      max = heap[1]
+      heap.pop()
+      return max
+
+  max_value = heap[0]
+  last_index = len(heap) - 1
+
+  # swap root with last element
+  heap[0], heap[last_index] = heap[last_index], heap[0]
+  heap.pop() # remove last element
+
+  if len(heap) == 2:  # if there are 3 element, then after pop, remain 2.
+    # don't waste time on sorting these two.
+    # just return
+    return max_value;
+  
+  current_index = 0 # it swap with root, so current index is 0
+  left_child_index = 1    
+  right_child_index = 2
+
+  # which child bigger?
+  if heap[left_child_index] > heap[right_child_index]:
+    child_bigger_one_index = left_child_index
+  else:
+    child_bigger_one_index = right_child_index
+  
+  #
+  # swap until this node in right position
+  #
+  while heap[current_index] < heap[child_bigger_one_index]: 
+    # smaller then one of bigger child node
+    heap[current_index], heap[child_bigger_one_index] = heap[child_bigger_one_index], heap[current_index] 
+    # Swap
+    
+    # after swap, we should get current node index and new parent index
+    current_index = child_bigger_one_index
+    left_child_index = current_index * 2 + 1
+    right_child_index = current_index * 2 + 2
+    
+    # if left child not exists,
+    if left_child_index >= len(heap)-1:
+      break;
+
+    # if left child exists, but right one not exists
+    if left_child_index >= len(heap)-1:
+      heap[-1], heap[-2] = heap[-2], heap[-1]
+      break;
+    
+    # both child exists
+    # 2 child, get bigger child, compare with it, and swap if nessaeary
+    # which child bigger?
+    if heap[left_child_index] > heap[right_child_index]:
+      child_bigger_one_index = left_child_index
+    else:
+      child_bigger_one_index = right_child_index
+  
+  return max_value
+
+
+#
+# 
+#
+heap_sort_array = [] # empty for now
+
+for x in range(len(heap)):
+  heap_sort_array.append( max_heap_remove() )
+
+print('heap sorted: ', heap_sort_array)
 
 
 
-#print()
-#print(a)
+
+
+
+
 
 
 
